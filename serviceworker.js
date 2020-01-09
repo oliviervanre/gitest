4232a42d9035d6d14f796b982f088d640268f8ab
oliviervanre <olivier.vanre@gmail.com>
2020-01-09 13:16:18 +0100

    9 janvier

--- serviceworker.js
+++ serviceworker.js
@@ -1,55 +1,59 @@
-const version = 'V0.12'
+const version = 'V0.2'
 const staticCacheName = version + 'staticfiles'
-addEventListener('install', (installEvent)=>{
+addEventListener('install', (installEvent) => {
     console.log('Le service worker est en installation...')
     skipWaiting()
     installEvent.waitUntil(
         caches.open(staticCacheName)
-        .then( staticCache =>{
+        .then(staticCache => {
             // fichiers souhaités en cache
             staticCache.addAll([
-                'assets/font.txt', 
+                'assets/font.txt',
                 'assets/icon.txt'
             ])
             console.log('mise en cache des fichiers, SW installé dans la version ' + version)
             // fichiers à mettre impérativement en cache
             return staticCache.addAll([
                 'css/stylesheet.css',
-                'js/javascript.js'
+                'js/javascript.js',
+                'offline.html'
             ])
         })
     )
 })
 addEventListener('activate', (activateEvent) => {
-            console.log('Le service worker est activé')
-            activateEvent.waitUntil(
-                caches.keys()
-                .then(cacheNames => {
-                        return Promise.all(
-                            cacheNames.map(cacheName => {
-                                if (cacheName != staticCacheName){
-                                    return caches.delete(cacheName)
-                                }
-                                })
-                        )
-                    })
-                    .then( () => {
-                        return clients.claim()
-                    })
+    console.log('Le service worker est activé')
+    activateEvent.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName != staticCacheName) {
+                        return caches.delete(cacheName)
+                    }
+                })
             )
+        })
+        .then(() => {
+            return clients.claim()
+        })
+    )
 })
-addEventListener('fetch', fetchEvent =>{
+addEventListener('fetch', fetchEvent => {
     const request = fetchEvent.request
     console.log(request)
     fetchEvent.respondWith(
         caches.match(request)
-        .then (responseFromCache => {
-            if(responseFromCache) {
+        .then(responseFromCache => {
+            if (responseFromCache) {
                 console.log('cache !')
                 return responseFromCache
-            } 
-                console.log('pas cache')
-                return fetch(request)
+            }
+            console.log('pas cache' + request.url)
+            return fetch(request)
+            .catch( error => {
+                return caches.match('/offline.html')
+            })
         })
     )
 })
